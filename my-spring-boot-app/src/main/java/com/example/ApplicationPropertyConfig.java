0d3deee198af363c31f5dc8dc3ed758d55cb4abd
Ashutosh Jaiswal <ashutoshjais98@gmai.com>
2025-01-23 19:48:54 +0530

    get secrets as env variables

--- my-spring-boot-app/src/main/java/com/example/ApplicationPropertyConfig.java
+++ my-spring-boot-app/src/main/java/com/example/ApplicationPropertyConfig.java
@@ -1,8 +1,12 @@
 package com.example;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +15,18 @@ import java.util.Map;
 public class ApplicationPropertyConfig {
 
     private Map<String, String> myMap = new HashMap<>();
+
+    @Value("${MY_SECRET_MAP:#{null}}")
+    private String mySecretMapJson;
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (mySecretMapJson != null) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, String> secretMap = objectMapper.readValue(mySecretMapJson, HashMap.class);
+            myMap.putAll(secretMap);
+        }
+    }
 
     public Map<String, String> getMyMap() {
         return myMap;
